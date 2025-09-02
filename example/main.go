6fb875a6a758d26e61d706cbeca9fdf3e9ce66f6
Hekmon <3418467+hekmon@users.noreply.github.com>
2025-09-02 17:00:25 +0200

    add example code

---
+++ example/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+
+	"github.com/hekmon/go-vobsub"
+)
+
+const (
+	// you must pass the .idx file but the .sub file must be present too !
+	idxFile = "/path/to/you/subtitle.idx"
+)
+
+func main() {
+	subs, err := vobsub.Decode(idxFile)
+	if err != nil {
+		panic(err)
+	}
+	for index, sub := range subs {
+		filename := fmt.Sprintf("sub_%03d.png", index+1)
+		fmt.Printf("Subtitle #%d: %s --> %s\n", index+1, sub.Start, sub.Stop)
+		if err = writeSub(filename, sub.Image); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func writeSub(filename string, img image.Image) (err error) {
+	file, err := os.Create(filename)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	return png.Encode(file, img)
+}
